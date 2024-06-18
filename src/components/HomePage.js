ae87f26737478beee6c2c382bd6c41a23d1dc114
System Administrator <shittuwasiuayodeji@gmail.com>
2024-06-18 17:42:38 +0200

    initial commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,54 @@
+import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import "./HomePage.css";
+import Navbar from "./NavBar";
+
+const HomePage = () => {
+  return (
+    <div className="home-page-container">
+      <div className="home-page-header">
+        <Navbar />
+      </div>
+      <div className="home-page">
+        <div>
+          <span>hey,</span>
+          <p>
+            this is<span> Deji</span>,
+          </p>
+        </div>
+        <span>a web developer.</span>
+      </div>
+      <div className="home-page-footer">
+        <a
+          href="https://github.com/ShittuAyodeji"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHubIcon />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ayodeji-shittu-685438a5/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedInIcon />
+        </a>
+        <a
+          href="https://www.instagram.com/ay0d0t/?hl=en"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InstagramIcon />
+        </a>
+        <a href="/">
+          <TwitterIcon />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
